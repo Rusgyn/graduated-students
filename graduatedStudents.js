2d72f001d8b6354c6bfbce2e9bdb1b4fb34f860c
rusgyn <rusgynmagsanay@gmail.com>
2024-09-07 01:39:12 -0400

    Implement the funcion that returns the status of a students, graduated or not yet.

--- graduatedStudents.js
+++ graduatedStudents.js
@@ -1,3 +1,11 @@
+/**
+ * Your goal is to look through the list of students. 
+ * - If a student has graduated, then your function should output a string literal informing you that the student has graduated and their diploma has been sent to their email. 
+ * - If they haven’t, the function should output a string literal informing you that the student has not graduated and no diploma will be sent.
+ * 
+ * Output: Regardless of the outcome, all string literals should be pushed in the array which the function will return at the end.
+ */
+
 const students = [
   {
     name: "Ankit",
@@ -29,7 +37,7 @@ const students = [
     grade: 75,
     email: "bernard@example.com",
     phone: "555-555-5555",
-    graduated: true
+    graduated: false
   },
   {
     name: "Kevin",
@@ -73,6 +81,23 @@ const students = [
   },
 ];
 
+
 const graduatedStudents = (listOfStudents) => {
-//Implement function
+  //Implement function
+  const areStudentsGraduated = [];
+
+  listOfStudents.map(({ name, age, grade, email, phone, graduated }) => {
+    graduated ? areStudentsGraduated.push(`${name} has graduated and their diploma will be sent to ${email}`) : areStudentsGraduated.push(`${name} has not graduated yet, no diploma will be sent`);
+  });
+
+  return areStudentsGraduated;
 };
+
+console.log(graduatedStudents(students));
+
+/** or below solution can be use without destructuring.
+ * 
+ * listOfStudents.forEach(student => { 
+    (student.graduated) ? areStudentsGraduated.push(`${student.name} has graduated and their diploma will be sent to ${student.email}.`) : areStudentsGraduated.push(`${student.name} has not graduated yet, no diploma will be sent.`);
+  });
+ */
